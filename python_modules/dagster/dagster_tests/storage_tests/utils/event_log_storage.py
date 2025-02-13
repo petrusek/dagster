eff0eae149e88f9e28a3fdac51cc1738f187cdd0
prha <philrha@gmail.com>
2025-02-12 21:30:16 -0800

    fix tests for changing initialize concurrency slot calls

--- python_modules/dagster/dagster_tests/storage_tests/utils/event_log_storage.py
+++ python_modules/dagster/dagster_tests/storage_tests/utils/event_log_storage.py
@@ -5284,6 +5284,8 @@ class TestEventLogStorage:
         if not self.can_set_concurrency_defaults():
             pytest.skip("storage does not support setting global op concurrency defaults")
 
+        assert not storage.initialize_concurrency_limit_to_default("foo")
+
         self.set_default_op_concurrency(instance, storage, 1)
 
         # initially there are no concurrency limited keys
@@ -5306,6 +5308,8 @@ class TestEventLogStorage:
         if not self.can_set_concurrency_defaults():
             pytest.skip("storage does not support setting global op concurrency defaults")
 
+        assert not storage.initialize_concurrency_limit_to_default("foo")
+
         self.set_default_op_concurrency(instance, storage, 1)
         assert instance.global_op_concurrency_default_limit == 1
         assert storage.get_pool_config().default_pool_limit == 1
@@ -5324,7 +5328,7 @@ class TestEventLogStorage:
         assert instance.global_op_concurrency_default_limit is None
         assert storage.get_pool_config().default_pool_limit is None
 
-        assert storage.initialize_concurrency_limit_to_default("foo")
+        assert not storage.initialize_concurrency_limit_to_default("foo")
         assert storage.get_concurrency_info("foo").slot_count == 0
 
     def test_asset_checks(
